6d4ee5861c03209d47d231712b142631e1565882
Skye <skye.grossman@gmail.com>
2021-12-20 14:21:02 -1000

    updated scroller

--- client/src/js/introScroller.js
+++ client/src/js/introScroller.js
@@ -1,21 +1,31 @@
 
-    
+// const intViewportWidth = window.innerWidth;
+// const intViewportHeight = window.innerHeight;
+// console.log( "width " + intViewportWidth );
+// console.log( "height " + intViewportHeight );
 
 
 
 export const scroller = window.addEventListener( 'scroll', function () {
-        const introScroll = document.querySelector( '.introImage' );
-        const valueA = window.scrollY;
-        const valueB = window.scrollY / 2;
-        introScroll.style.transform = "translate3d(" + valueB + "px, " + valueA + "px, 0)";
+    const introScroll = document.querySelector( '.introImage' );
+    const valueA = window.scrollY;
+    const valueB = window.scrollY / 2;
+    introScroll.style.transform = "translate3d(" + valueB + "px, " + valueA + "px, 0)";
     } );
 
 export const bioScroller = window.addEventListener( 'scroll', function () {
     const bioScroll = document.querySelector( '.bio_content' );
+    const intViewportWidth = window.innerWidth;
+    const intViewportHeight = window.innerHeight;
+    if ( intViewportHeight > 1100 ) {
+        bioScroll.style.transform = "translate3d(0, 0, 0)";
+        bioScroll.style.opacity = 1;
+
+    }
     const value2 = window.scrollY;
     let num = 0;
     let opaNum = 0;
-    if ( value2 < 700 ) {
+    if ( value2 < 700 && intViewportHeight < 1100 ) {
         num = 700 - value2;
         bioScroll.style.transform = "translate3d(-" + num + "px, 0, 0)";
         opaNum = ( value2 - 500 ) / 200;
@@ -29,11 +39,34 @@ export const bioScroller = window.addEventListener( 'scroll', function () {
 export const projectScroller = window.addEventListener( 'scroll', function() {
     const projectScroll = document.querySelector( '.projects' );
     let value3 = window.scrollY;
-    if ( value3 < 2100 ) {
-        let opaNum2 = ( value3 - 1700 ) / 400;
+    const intViewportWidth = window.innerWidth;
+    const intViewportHeight = window.innerHeight;
+    let opaNum2 = 0;
+    //console.log( intViewportWidth );
+    console.log( intViewportHeight );
+    if ( value3 < 2100 && intViewportHeight < 1100) {
+        opaNum2 = ( value3 - 1700 ) / 400;
         projectScroll.style.opacity = opaNum2;
-    } else if ( value3 > 2100 ) {
-        projectScroll.style.opacity = 1;
+        console.log( value3 );
+        // console.log( opaNum2 );
+    } 
+    if ( value3 < 1600 && intViewportHeight > 1100 && intViewportHeight < 1600 ) {
+        opaNum2 = ( value3 - 1200 ) / 400;
+        projectScroll.style.opacity = opaNum2;
+        console.log( value3 );
+        console.log( opaNum2 );
+    }
+    if ( value3 < 1000 && intViewportHeight > 1600 && intViewportHeight < 2100) {
+        opaNum2 = ( value3 - 600 ) / 400;
+        projectScroll.style.opacity = opaNum2;
+        console.log( value3 );
+        console.log( opaNum2 );
+    }
+    if ( value3 < 600 && intViewportHeight > 2100 ) {
+        opaNum2 = ( value3 - 200 ) / 400;
+        projectScroll.style.opacity = opaNum2;
+        console.log( value3 );
+        console.log( opaNum2 );
     }
 } );
 
